6d4bcc202926f1ae7152320ece11086fe898c324
MaGloCon <mgcontrada@gmail.com>
2024-06-22 16:17:03 +0200

    Set up Atatus for SPA performance and error monitoring
    Imported and initialized the Atatus SDK in the index.js file and configured the Atatus SDK with the project-specific API key.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+import * as atatus from 'atatus-spa';
+atatus.config('fa2d1580db3045d2bf6c79127a16b44a').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,3 +25,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
